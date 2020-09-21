b0d39c9e797bc1348564fa7f920aec85a0810210
pangqixin <123456>
2020-09-21 15:59:59 +0800

    1

--- robot/models.py
+++ robot/models.py
@@ -4,7 +4,31 @@ from django.db import models
 from django.db import models
 
 
-class Exam(models.Model):
-    exam_id = models.IntegerField()
-    exam_name = models.TextField()
-    exam_create_time = models.TextField()
+class ZSPapers(models.Model):
+    # 时间
+    data_pid = models.CharField(max_length=15)
+    # 试卷名称
+    exam_title = models.TextField()
+    # 试卷创建时间
+    exam_time = models.CharField(max_length=15)
+    # 标签
+    tags = models.TextField()
+    # 单选题
+    choice_question = models.TextField()
+    # 判断题
+    judge_question = models.TextField()
+    # 填空题
+    completion_question = models.TextField()
+    # 简答题
+    short_answer_question = models.TextField()
+    # 名词解释
+    explanation_question = models.TextField()
+    # 案例分析
+    analysis_question = models.TextField()
+    # 多选题
+    much_choice_question = models.TextField()
+    # 论述题
+    longer_question = models.TextField()
+
+
+
--- robot/views.py
+++ robot/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-from robot.models import  Exam
+from django.shortcuts import render, HttpResponse
 # Create your views here.
-
-
+from django.core import serializers
+from robot.models import ZSPapers
+import json
+import time
 def show_exam_handle_index_controller(request):
     return render(request, "robot/exam.html")
 
+
+def get_exam_all_controller(request):
+    time.sleep(20)
+    data = {
+        "status": "0"
+    }
+    return HttpResponse(json.dumps(data))
+
+
+def get_exam_new_controller(request):
+    # count = ZSPapers().objects.count()
+    # print(count)
+    data = {
+        "status": "0"
+    }
+    return HttpResponse(json.dumps(data))
 
