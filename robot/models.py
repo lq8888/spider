d108cc5ec1d62d1748ac3bb67b6083709def5b40
pangqixin <123456>
2020-09-22 17:37:36 +0800

    models题型表

--- robot/models.py
+++ robot/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db import models
 
 
+# 中师试卷
 class ZSPapers(models.Model):
     # 试卷id
     data_pid = models.CharField(max_length=15, primary_key=True)
@@ -33,4 +34,77 @@ class ZSPapers(models.Model):
     other_question = models.TextField()
 
 
+# 单选题
+class ZSChoiceQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 题目选项
+    question_option = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
 
+
+# 判断题
+class ZSJudgeQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
+
+
+# 填空题
+class ZSCompletionQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
+
+
+# 简答题
+class ZSShortAnswerQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
+
+
+# 名词解释题
+class ZSExplanationQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
+
+
+# 案例分析题
+class ZSAnalysisQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
+
+
+# 多选题
+class ZSMuchChoiceQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 题目选项
+    question_option = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
+
+
+# 论述题
+class ZSLongerQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
+
+
+# 其他题
+class ZSOtherQuestion(models.Model):
+    # 题目标题
+    question_title = models.TextField()
+    # 试卷id
+    paper_id = models.ForeignKey("ZSPapers", on_delete=models.DO_NOTHING)
